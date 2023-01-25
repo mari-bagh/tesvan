b6e0aeaff3893edf151ef312d30280e7614a5964
mari-bagh <maribaghdasaryan0@gmail.com>
2023-01-25 12:20:12 +0400

    Developer Skills

--- script.js
+++ script.js
@@ -1,56 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
-const a = [45,5,1,6]
-const temps = temperatures.concat(a)
-const calcTempAmplitude = function (temps) {
-    let max = temps[0];
-    let min = temps[0];
-    for (let i = 0; i < temps.length; i++){
-        let  currTemp = temps[i];
-        if(typeof currTemp !== 'number') continue;
-     if(currTemp > max) max = currTemp;
-     if(currTemp < min) min = currTemp;
-
-    }
-    console.log(max, min);
-    return max - min;
-}
-const amplitude = calcTempAmplitude(temps);
-console.log(amplitude);
-
-const calcTempAmplitudeNew = function (t1, t2) {
-    const temps = t1.concat(t2);
-    console.log(temps);
-
-    let max = temps[0];
-    let min = temps[0];
-    for (let i = 0; i < temps.length; i++){
-        let  currTemp = temps[i];
-        if(typeof currTemp !== 'number') continue;
-        if(currTemp > max) max = currTemp;
-        if(currTemp < min) min = currTemp;
-
-    }
-    console.log(max, min);
-    return max - min;
-}
-const amplitudeNew = calcTempAmplitudeNew(temperatures, [2,6,78,45,1]);
-console.log(amplitudeNew);
-
-const measureKelvin = function () {
-    const measurement = {
-        type : 'temp',
-        unit: 'celsius',
-        value: Number(prompt('Degrees celsius:')),
-    };
-    console.table(measurement);
-    const kelvin = measurement.value + 273;
-    return kelvin;
-};
-console.log(measureKelvin())
-
 //CHALLENGE #1
 
 let temp = [17, 21, 23];
